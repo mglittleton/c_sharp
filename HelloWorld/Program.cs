b533f6a9d16108516334c45c8e02c54420ee73df
Michael Littleton <michaelglittleton@gmail.com>
2021-01-11 19:05:48 -0500

    Working with 'int' variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 13;
+            int num2 = 45;
+            int sum = num1 + num2;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum + '.');
+
+
             Console.Read();
         }
     }
