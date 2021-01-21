6641f6f6ae9449f9f0f0bc22963b329a1a6a9075
Michael Littleton <michaelglittleton@gmail.com>
2021-01-21 12:41:38 -0500

    Intro to array structures

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -7,14 +7,35 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
         }
+
+
+
+        // ----- Intro to class structures -----
+        //static void Main(string[] args)
+        //{
+        //    var john = new Person();
+        //    john.FirstName = "John";
+        //    john.LastName = "Smith";
+        //    john.Introduce();
+
+        //    Calculator calculator = new Calculator();
+        //    var result = calculator.Add(1, 2);
+        //    Console.WriteLine(result);
+        //}
     }
 }
