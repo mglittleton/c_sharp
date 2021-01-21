58b5fd1604ebb4feb9e1d5d05c062a9a1f7a3fc9
Michael Littleton <michaelglittleton@gmail.com>
2021-01-21 13:05:39 -0500

    Intro to enum structures

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -3,25 +3,49 @@ using CSharpFundamentals.Math;
 
 namespace CSharpFundamentals
 {
+    public enum ShippingMethods
+    {
+        Regular = 1,
+        Registered = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var method = ShippingMethods.Express;
+            Console.WriteLine((int)method);
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var methodId = 3;
+            Console.WriteLine((ShippingMethods)methodId);
 
-            var flags = new bool[3];
-            flags[0] = true;
+            Console.WriteLine(method.ToString());
 
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
-
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethods)Enum.Parse(typeof(ShippingMethods), methodName);
+            Console.WriteLine(shippingMethod);
         }
+
+
+        // ----- Intro to array structures -----
+        //static void Main(string[] args)
+        //{
+        //    var numbers = new int[3];
+        //    numbers[0] = 1;
+
+        //    Console.WriteLine(numbers[0]);
+        //    Console.WriteLine(numbers[1]);
+        //    Console.WriteLine(numbers[2]);
+
+        //    var flags = new bool[3];
+        //    flags[0] = true;
+
+        //    Console.WriteLine(flags[0]);
+        //    Console.WriteLine(flags[1]);
+        //    Console.WriteLine(flags[2]);
+
+        //    var names = new string[3] { "Jack", "John", "Mary" };
+        //}
 
 
 
